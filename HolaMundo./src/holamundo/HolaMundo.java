39c0aa6373470861753ac3e7ee3209e0b640aea4
jose <jose@jose-VirtualBox>
2019-02-07 19:39:20 +0100

    Subiendo tercer cambio en proyecto HolaMundo colores

--- HolaMundo./src/holamundo/HolaMundo.java
+++ HolaMundo./src/holamundo/HolaMundo.java
@@ -15,7 +15,11 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+           System.out.println("ADIOS");
+           System.out.println("VERDE");
+           System.out.println("ROJO");
+           
+           
     }
     
 }
